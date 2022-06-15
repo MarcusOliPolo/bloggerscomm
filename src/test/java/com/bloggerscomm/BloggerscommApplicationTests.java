29a336a602de607b14b7c6219f3193d99635abce
MarcusOliPolo <polomcak8@gmail.com>
2022-06-15 19:19:50 +0200

    We are on the beginning of motherfucker road

---
+++ src/test/java/com/bloggerscomm/BloggerscommApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bloggerscomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggerscommApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assert true;
+	}
+
+}
